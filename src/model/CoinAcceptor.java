f747997b6b9c9e932d53d1214b984b694dbc1742
Maks <maximos717765@gmail.com>
2025-12-11 14:07:13 +0600

    Remade CoinAcceptor according to new interface and implemented methods accordingly

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,39 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements MoneyAcceptor {
+
+
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
+
+    @Override
     public int getAmount() {
-        return amount;
-    }
+        return amount;}
 
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    @Override
+    public void addAmount() {
+        System.out.println("Вы бросили монетку +10");
+        amount += 10;
+    }
+
+    @Override
+    public void spendAmount(int price) {
+        amount -= price;
+    }
+    @Override
+    public void showInterface() {
+        System.out.println(" a - Пополнить баланс (бросить монету)");
+    }
+
+
+
+
 }
